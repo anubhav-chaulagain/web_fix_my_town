bdaabbbe80d099d3bcc8a164bf17448e58d232df
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2025-12-27 09:11:31 +0545

    added zod schema to signup form

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { useForm } from "react-hook-form";
+import { SignupForm, signupSchema } from "./schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function SignupPage() {
-    const {register, handleSubmit, formState:{errors}} = useForm({
-        values : {username:"", email:"", password:"", role:"Citizen"}
+    const {register, handleSubmit, formState:{errors}} = useForm<SignupForm>({
+        resolver: zodResolver(signupSchema),
+        values : {username:"", email:"", password:"", role:"citizen"}
     });
     return (
         <form onSubmit={handleSubmit(()=>{})}>
@@ -12,26 +15,27 @@ export default function SignupPage() {
             <h2>Create an new account!</h2>
             <div>
                 <label>Username</label>
-                <input {...register("username", {required: "Username is required!", minLength:{value: 3, message:"Minimun required length is 3!"}})}/>
+                <input {...register("username")}/>
                 <p>{errors.username?.message}</p>
             </div>
             <div>
                 <label>Email</label>
-                <input {...register("email", {required: "Email is required!", pattern: {value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message:"Invalid email format!"}})}/>
+                <input {...register("email")}/>
                 <p>{errors.email?.message}</p>
             </div>
             <div>
                 <label>Password</label>
-                <input {...register("password", {required: "Password is required!", minLength:{value: 8, message:"Minimun length is 8!"},
-                maxLength: {value:20, message:"Maximum length is 20!"}})}/>
+                <input type="password" {...register("password")}/>
                 <p>{errors.password?.message}</p>
             </div>
             <div>
+                <label>Role</label>
                 <select name="role" id="role">
-                    <option value={"citizen"}>Citizen</option>
-                    <option value={"authority"}>Authority</option>
-                    <p>{errors.role?.message}</p>
+                    <option value="citizen">Citizen</option>
+                    <option value="authority">Authority</option>
                 </select>
+                <p>{errors.role?.message}</p>
+
             </div>
             <div>
                 <button type="submit">Sumbit</button>
---
+++ app/(auth)/signup/schema.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+export const signupSchema = z.object({
+    username: z.string().min(4, "Minimum length is 4!"),
+    email: z.email({error: "Invalid email format!"}),
+    password: z.string().min(6, "Password should be at least 6 characters long!").max(20, "Password should be at max 20 characters!"),
+    role: z.enum(["citizen", "authority"]),
+});
+
+export type SignupForm = z.infer<typeof signupSchema>;
