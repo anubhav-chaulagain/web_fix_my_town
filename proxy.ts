6aab79fac3e7d778ab30278ab3483d0cecf52956
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2026-02-03 20:50:04 +0545

    proxy

---
+++ proxy.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getAuthToken, getUserData } from "./lib/cookie";
+
+const publicPaths = ["/login", "/signup", "/forgot-password"];
+const adminPaths = ["/admin"]
+const userPaths = ["/user"]
+
+export async function proxy(req: NextRequest){
+    const { pathname } = req.nextUrl;
+    
+    const token = await getAuthToken();
+    const user = token ? await getUserData() : null;
+
+    const isPublicPath = publicPaths.some((path) => pathname.startsWith(path));
+    
+    const isAdminPath = adminPaths.some((path) => pathname.startsWith(path));
+
+    const isUserPath = userPaths.some((path) => pathname.startsWith(path));
+
+    if(!token && !isPublicPath){
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    if(token && user){
+        if(isAdminPath && user.role !== "admin"){
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+        if(isUserPath && user.role !== "user" && user.role !== "admin"){
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    }
+
+    if(isPublicPath && user){
+        return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        // which path should be checked inside proxy
+        "/admin/:path*",
+        "/user/:path*",
+        "/login",
+        "/register"
+    ],
+}
