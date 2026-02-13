e5ec343f0e9a3db50fe0146a5a299ad3436afe25
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2026-02-13 08:41:07 +0545

    endpoints pass reset and reset pass

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -4,6 +4,8 @@ export const API = {
     AUTH: {
         REGISTER: '/api/auth/register',
         LOGIN: '/api/auth/login',
+        REQUEST_PASSWORD_RESET: '/api/auth/request-password-reset',
+        RESET_PASSWORD: (token: string) => `/api/auth/reset-password/${token}`,
     },
     ADMIN: {
         CREATE: "/api/admin/users",
