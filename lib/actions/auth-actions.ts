19286d68e33f301f0c2af6b7e5fb1785fd1e3167
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2026-02-13 08:48:38 +0545

    auth api actions for ReqResetPass and ResetPass

--- lib/actions/auth-actions.ts
+++ lib/actions/auth-actions.ts
@@ -1,6 +1,7 @@
 // server side processing
 'use server'
-import { register, login } from "../api/auth";
+import { success } from "zod";
+import { register, login, requestPasswordReset, resetPassword } from "../api/auth";
 import { setAuthToken, setUserData } from "../cookie";
 
 export const handleRegister = async ( formData: any ) => {
@@ -44,3 +45,33 @@ export const handleLogin = async ( formData: any ) => {
         return { success: false, message: err.message || "Login failed"};
     }
 }
+
+export const handleRequestPasswordReset = async (email: string) => {
+    try {
+        const response = await requestPasswordReset(email);
+        if (response.success) {
+            return {
+                success: true,
+                message: "Password reset email sent successfully"
+            }
+        }
+        return { success: false, message: response.message || 'Request password reset failed' }
+    } catch (error: Error | any) {
+        return { success: false, message: error.message || 'Request password reset action failed' };
+    }
+}
+
+export const handleResetPassword = async (token: string, newPassword: string) => {
+    try {
+        const response = await resetPassword(token, newPassword);
+        if (response.success) {
+            return {
+                success: true,
+                message: 'Password has been reset successfully'
+            }
+        }
+        return { success: false, message: response.message || 'Reset password failed' }
+    } catch (error: Error | any) {
+        return { success: false, message: error.message || 'Reset password action failed' }
+    }
+};
